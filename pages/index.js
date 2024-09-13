f37f45b3fc4cfc878371ed52ad11f68dfe02ac0d
Dev.Huang <devhuang000@outlook.com>
2024-09-13 23:25:24 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,7 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <span className={styles.highlight}>Next.js</span>!
-        </h1>
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="/vercel.svg" alt="Vercel Logo" />
-          </span>
-        </a>
-      </footer>
+      <h1 className={styles.title}>Hello World</h1>
     </div>
   );
 }
