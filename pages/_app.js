b45619808f530318033b11377897e30fdca0d76b
Evan Huang <evan.huang000@proton.me>
2024-11-02 12:23:47 +0800

    Update and rename _app.tsx to _app.js

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+// pages/_app.js
+import { ClerkProvider } from '@clerk/nextjs';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
+
+export default MyApp;
