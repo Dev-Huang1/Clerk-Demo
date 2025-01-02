328438f33b648a5b2fb3fa43cea3dd55f1db4d30
Evan Huang <evan.huang000@proton.me>
2025-01-03 00:57:38 +0800

    Create shadcnui.js

---
+++ pages/shadcnui.js
@@ -0,0 +1,9 @@
+import { Button } from '@shadcn/ui';
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <Button variant="primary">Hello from ShadCN UI</Button>
+    </div>
+  );
+}
